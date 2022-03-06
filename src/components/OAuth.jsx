8ba458c7ebb18e00bc4664aa20d5c43035393ff8
Federica Fiorenza <20091413@mail.wit.ie>
2022-03-06 15:56:43 +0000

    signin page editing icons display

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
-import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, GithubAuthProvider, TwitterAuthProvider} from 'firebase/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider, MicrosoftAuthProvider, GithubAuthProvider, TwitterAuthProvider} from 'firebase/auth'
 import { doc, setDoc, getDoc, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import googleIcon from '../assets/svg/googleIcon.svg'
 import facebookIcon from '../assets/svg/facebookIcon.svg'
+import microsoftIcon from '../assets/svg/microsoftIcon.svg'
 import githubIcon from '../assets/svg/githubIcon.svg'
 import twitterIcon from '../assets/svg/twitterIcon.svg'
 
@@ -141,18 +142,32 @@ function OAuth() {
   return (
     <div className='socialLogin'>
       <p>Sign {location.pathname === '/sign-up' ? 'up' : 'in'} with </p>
+      <table>
+    <tr>
+      <th>
       <button className='socialIconDiv' onClick={onGoogleClick}>
         <img className='socialIconImg' src={googleIcon} alt='google' />
       </button>
+      </th>
+      <th>
       <button className='socialIconDiv' onClick={onFacebookClick}>
         <img className='socialIconImg' src={facebookIcon} alt='facebook' />
       </button>
+      </th>
+      <th>
+      <button className='socialIconDiv'>
+        <img className='socialIconImg' src={microsoftIcon} alt='microsoft' />
+      </button></th>
+      <th>
       <button className='socialIconDiv' onClick={onGitHubClick}>
         <img className='socialIconImg' src={githubIcon} alt='github' />
-      </button>
+      </button></th>
+      <th>
       <button className='socialIconDiv' onClick={onTwitterClick}>
         <img className='socialIconImg' src={twitterIcon} alt='twitter' />
-      </button>
+      </button></th>
+      </tr>
+      </table>
     </div>
   )
 }
