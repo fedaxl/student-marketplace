a8325cfaafc96d1fbcd1cf2e8c19bd3769359661
Federica Fiorenza <20091413@mail.wit.ie>
2022-04-15 00:50:40 +0100

    updated Stripe checkout page

--- server.js
+++ server.js
@@ -1,43 +1,35 @@
-var express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//const bodyParser = require('body-parser');
 //const inventoryData = require('.\inventory.json');
-const jquery = require("jquery");
+//const jquery = require("jquery");
 const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
-const { json } = require("body-parser");
+//const { json } = require("body-parser");
  
 const stripe = require('stripe')('sk_test_51Kd0SWGQMXnbID09dzc2hKp5QzeM871KOKgDGa6OkP87QWrwm6ZmhPtsMoPNX1YHfnSkL7fHNdXGcNlMR7gXWpt400T9xf1fwp');
  
 const braintree = require("braintree");
 
-var app = express();
-var port = process.env.PORT || 4000;
- 
-// enable CORS
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-// parse application/json
-app.use(bodyParser.json());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
  
 // confirm the paymentIntent Stripe
-app.post('/pay', async (request, response) => {
-  try {
-    // Create the PaymentIntent Stripe
-    let intent = await stripe.paymentIntents.create({
-      payment_method: request.body.payment_method_id,
-      description: "Test payment",
-      amount: request.body.amount * 10.00,
-      currency: 'eur',
-      confirmation_method: 'manual',
-      confirm: true
-    });
-    // Send the response to the client
-    response.send(generateResponse(intent));
-  } catch (e) {
-    // Display error on client
-    return response.send({ error: e.message });
-  }
+// same api we used in the frondend
+app.post("/api/stripe/charge", async (req, resp) => {
+  const { token, currency, price } = req.body;
+  const charge = await stripe.charges.create({
+    amount: price,
+    currency,
+    source: token,
+  });
+
+  if (!charge) throw new Error("charge unsuccessful");
 });
 
 const gateway = new braintree.BraintreeGateway({
--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,143 +1,54 @@
-import React, { useState } from 'react';
-import {
-  useStripe, useElements,
-  CardNumberElement, CardExpiryElement, CardCvcElement
-} from '@stripe/react-stripe-js';
-import { stripePaymentMethodHandler } from './script';
+import React from 'react'
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import axios from "axios";
 
-const CARD_ELEMENT_OPTIONS = {
-  style: {
-    base: {
-      lineHeight: "27px",
-      color: "#212529",
-      fontSize: "1.1rem",
-      "::placeholder": {
-        color: "#aab7c4",
-      },
-    },
-    invalid: {
-      color: "#fa755a",
-      iconColor: "#fa755a",
-    },
-  },
-};
-
-export default function CheckoutForm(props) {
-  const [loading, setLoading] = useState(false);
-  const [errorMsg, setErrorMsg] = useState('');
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-
+const CheckoutForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-
-  const handleSubmit = async (event) => {
-    // We don't want to let default form submission happen here,
-    // which would refresh the page.
-    event.preventDefault();
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     if (!stripe || !elements) {
-      // Stripe.js has not yet loaded.
-      // Make sure to disable form submission until Stripe.js has loaded.
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
       return;
     }
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
 
-    setLoading(true);
-    setErrorMsg('');
+    // use stripe.createToken to get a unique token for the card
+    const { error, token } = await stripe.createToken(cardElement);
 
-    const paymentMethodObj = {
-      type: 'card',
-      card: elements.getElement(CardNumberElement),
-      billing_details: {
-        name,
-        email
-      },
-    };
-    const paymentMethodResult = await stripe.createPaymentMethod(paymentMethodObj);
-
-    stripePaymentMethodHandler({
-      result: paymentMethodResult,
-      amount: props.amount
-    }, handleResponse);
-  };
-
-  // callback method to handle the response
-  const handleResponse = response => {
-    setLoading(false);
-    if (response.error) {
-      setErrorMsg(typeof response.error === 'string' ? response.error : response.error.message);
-      return;
+    if (!error) {
+      // Backend is not implemented yet, but once there isn’t any errors,
+      // you can pass the token and payment data to the backend to complete
+      // the charge
+      axios
+        .post("http://localhost:5000/api/stripe/charge", {
+          token: token.id,
+          currency: "EUR",
+          price: 1000, // or 10 pounds (10*100). Stripe charges with the smallest price unit allowed
+        })
+        .then((resp) => {
+          alert("Your payment was successful");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      console.log(error);
     }
-    props.setPaymentCompleted(response.success ? true : false);
   };
 
   return (
-    <React.Fragment>
-      <h4 className="d-flex justify-content-between align-items-center mb-3">
-        <span className="text-muted">Pay with card</span>
-      </h4>
-      <form onSubmit={handleSubmit}>
-
-        <div className="row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="cc-name">Name on card</label>
-            <input
-              id="cc-name"
-              type="text"
-              className="form-control"
-              value={name}
-              onChange={e => setName(e.target.value)}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label htmlFor="cc-email">Email</label>
-            <input
-              id="cc-email"
-              type="text"
-              className="form-control"
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-12 mb-3">
-            <label htmlFor="cc-number">Card Number</label>
-            <CardNumberElement
-              id="cc-number"
-              className="form-control"
-              options={CARD_ELEMENT_OPTIONS}
-            />
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="expiry">Expiration Date</label>
-            <CardExpiryElement
-              id="expiry"
-              className="form-control"
-              options={CARD_ELEMENT_OPTIONS}
-            />
-          </div>
-          <div className="col-md-6 mb-3">
-            <label htmlFor="cvc">CVC</label>
-            <CardCvcElement
-              id="cvc"
-              className="form-control"
-              options={CARD_ELEMENT_OPTIONS}
-            />
-          </div>
-        </div>
-
-        <hr className="mb-4" />
-        <button className="btn btn-dark w-100" type="submit" disabled={loading}>
-          {loading ? <div className="spinner-border spinner-border-sm text-light" role="status"></div> : `PAY €${props.amount}`}
-        </button>
-        {errorMsg && <div className="text-danger mt-2">{errorMsg}</div>}
-      </form>
-    </React.Fragment>
+    <div>
+    <form onSubmit={handleSubmit}>
+      <CardElement />
+      <button>PAY</button>
+    </form>
+    </div>
   );
-}
+};
+
+export default CheckoutForm;
